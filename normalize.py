d0aa6f178d29eb784f2109aa0246514c2c6ccc80
nicobokha <nebokha@umich.edu>
2022-08-28 12:34:32 -0400

    Finished z score normalization

--- normalize.py
+++ normalize.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-def zScoreNorm(x, x_fold_mean, x_fold_std, y_fold_mean, y_fold_std, isOutput):
+def zScoreNorm(elem, x_fold_mean, x_fold_std, y_fold_mean, y_fold_std, isOutput):
     if isOutput: 
-        return (x - y_fold_mean) / y_fold_mean
+        return (elem - y_fold_mean) / y_fold_std
     else:
-        return (x - x_fold_mean) / x_fold_std
+        return (elem - x_fold_mean) / x_fold_std
 
 
 
